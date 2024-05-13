d308d1d0ba550cafcb400ad147bedf8f279ffa38
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2024-05-13 15:57:30 +0100

    Removed second app configuration

--- main.py
+++ main.py
@@ -21,19 +21,12 @@ logging.basicConfig(
 
 load_dotenv()
 
-try:
-    logging.info("Trying Bot 1")
-    app = App(
-        token=os.environ.get("SLACK_BOT_TOKEN"),
-        signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-    )
-except Exception as e:
-    logging.info("Trying Bot 2")
-    logging.error(f"Error authenticating with workspace 1: {e}")
-    app = App(
-        token=os.environ.get("SLACK_BOT_TOKEN_1"),
-        signing_secret=os.environ.get("SLACK_SIGNING_SECRET_1")
-    )
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
 
 
 def post_to_general(client, text, blocks=None):
